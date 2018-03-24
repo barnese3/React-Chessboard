79869ff5fe8ca2126c2c426008fff64a620c2374
Elise Barnes <barnese3@go.stockton.edu>
2018-03-24 14:20:48 -0400

    Board, Squares, and Knight set-up

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Board from './components/Board';
+import { observe } from './components/Game';
+import registerServiceWorker from './registerServiceWorker';
+
+const rootEl = document.getElementById('root');
+
+observe(knightPosition =>
+  ReactDOM.render(
+    <Board knightPosition={knightPosition} />,
+    rootEl
+  )
+);
+
+registerServiceWorker();
